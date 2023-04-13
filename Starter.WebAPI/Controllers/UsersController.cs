d28f3791b969f5b15347a1be047818b941999fbb
Yannick Dufils <yannick.dufils@procedureflow.com>
2023-04-13 10:12:05 -0300

    Users Renaming

---
+++ Starter.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using Starter.Application.Features.UserFeatures.CreateUser;
+using Starter.Application.Features.UserFeatures.GetAllUser;
+using Starter.Application.Features.UserFeatures.GetUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Starter.WebAPI.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UsersController : ControllerBase
+{
+  private readonly IMediator mediator;
+
+  public UsersController(IMediator mediator)
+  {
+    this.mediator = mediator;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<GetAllUserResponse>> GetAll(CancellationToken cancellationToken)
+  {
+    var response = await mediator.Send(new GetAllUserRequest(), cancellationToken);
+    return Ok(response);
+  }
+
+  [HttpGet]
+  [Route("{id:guid}")]
+  public async Task<ActionResult<GetUserResponse>> Get(Guid id, CancellationToken cancellationToken)
+  {
+    var response = await mediator.Send(new GetUserRequest(id), cancellationToken);
+    return Ok(response);
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<CreateUserResponse>> Create(CreateUserRequest request,
+      CancellationToken cancellationToken)
+  {
+    var response = await mediator.Send(request, cancellationToken);
+    return Ok(response);
+  }
+}
